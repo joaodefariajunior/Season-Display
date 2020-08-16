452b0bb832f6375fd788836a3b7218f68f9dd621
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-08-16 19:09:09 +0200

    Updates

---
+++ precache-manifest.27aa9fc6ba5e678262a3035a4b5533e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ecaa437c880a880ed4aaf5e0bc583e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "019279a7ffe80d9e112e",
+    "url": "./static/css/main.e8edc3b6.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "./static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019279a7ffe80d9e112e",
+    "url": "./static/js/main.20e3f2d4.chunk.js"
+  },
+  {
+    "revision": "4287c02a91c94e1f1786",
+    "url": "./static/js/runtime-main.744eb9e3.js"
+  }
+]);
